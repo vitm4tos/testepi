50cc2149ec5914f81e51c54923e7dcbebc2d1239
Hellen <hellen.matos@escolar.ifrn.edu.br>
2025-04-24 10:01:01 -0300

    Create testehelloworld.py

---
+++ testehelloworld.py
@@ -0,0 +1,4 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello World")
+Hello World
